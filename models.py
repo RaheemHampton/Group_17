0ba42ee3e555e9e3ddf8e89d7ed036ce05be643b
mpaloian <78007827+mpaloian@users.noreply.github.com>
2021-04-03 11:46:08 -0400

    updated models.py - updated event and rsvp tables

--- models.py
+++ models.py
@@ -1,23 +1,38 @@
 from database import db
+from datetime import datetime
 
 # Please update
 class Event(db.Model):
-    id = db.Column("id", db.Integer, primary_key=True)
-    title = db.Column("title", db.String(200))
-    text = db.Column("text", db.String(100))
-    date = db.Column("date", db.String(50))
+    __tablename__ = "Event"
+    id = db.Column("EventID", db.Integer, primary_key=True)
+    UserID = db.Column("UserID", db.Integer, db.ForeignKey('User.UserID'), nullable=False)
+    
+    eventName = db.Column("eventName", db.String(150))
+    dateTime = db.Column("dateTime", db.DateTime(100))
+    location = db.Column("location", db.String(100))
+    description = db.Column("description", db.Text(500))
 
-    def __init__(self, title, text, date):
-        self.title = title
-        self.text = text
-        self.date = date
+    # relations
+    rvsps = db.relationship('RSVP')
+
+    def __init__(self, UserID, eventName, dateTime, location, description):
+        self.UserID = UserID
+        self.eventName = eventName
+        self.dateTime = dateTime
+        self.location = location
+        self.description = description 
 
 class User(db.Model):
-    id = db.Column("id", db.Integer, primary_key=True)
+    __tablename__ = "User"
+    id = db.Column("UserID", db.Integer, primary_key=True)
     email = db.Column("email", db.String(100))
     firstName = db.Column("firstName", db.String(100))
     lastName = db.Column("lastName", db.String(100))
-    password = db.Column("password", db.String(100))
+    password = db.Column("password", db.String(100), nullable=False)
+
+    # relations
+    events = db.relationship('event')
+    rvsps = db.relationship('RSVP')
 
     def __init__(self, email, firstName, lastName, password):
         self.email = email
@@ -30,3 +45,15 @@ class User(db.Model):
             return True
         else:
             return False
+
+
+class RSVP(db.Model):
+    __tablename__ = "RSVP"
+    id = db.Column("id", db.Integer, primary_key=True)
+    UserID = db.Column("UserID", db.Integer, db.ForeignKey('User.UserID'), nullable=False)
+    EventID = db.Column("EventID", db.Integer, db.ForeignKey('Event.EventID'), nullable=False)
+
+
+    def __init__(self, UserID, eventID):
+        self.UserID = UserID
+        self.EventID = EventID
